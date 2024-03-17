fb3c66094302d36110c21987de2926ffdc405cee
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2024-03-17 13:59:18 -0700

    moving stuff out

--- python_scripts/list_alphabetizer.py
+++
@@ -1,8 +0,0 @@
-def user_input():
-    print("paste words serperated by spaces here: ")
-    list = str(input()).split(" ")
-    print("sorted list:")
-    list.sort()
-    print(list)
-    user_input()
-user_input()
