46983cc26bee7b6e97fbfa664c587c251fe2f6c5
Moonflower2022 <harrisonq125@gmail.com>
2025-02-16 09:48:27 -0800

    added random checker

---
+++ prime_sum_of_two_squares.py
@@ -0,0 +1,23 @@
+from math import isqrt
+
+def is_prime(n):
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    limit = isqrt(n)
+    for i in range(5, limit+1, 6):
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+    return True
+
+def is_sum_of_two_squares(n):
+    return n % 4 != 3
+
+if __name__ == "__main__":
+    limit = 1000000
+    num_good_primes = 0
+    for n in range(1, limit):
+        if is_prime_optimized(n) and is_sum_of_two_squares(n):
+            num_good_primes += 1
+    print(f"Number of good primes up to {limit}: {num_good_primes}")
